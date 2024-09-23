5d4e5e6e57b960c6ccff2ee04d58780b8036d1dd
Radaelli.20122 <Radaelli.20122@LABI1-6PC22>
2024-09-23 09:51:00 +0200

    Aggiungere i file di progetto.

---
+++ Crud con Seria-Deseria/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Crud_con_Seria_Deseria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(116, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Read";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(211, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(306, 27);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(25, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(356, 225);
+            this.listBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(211, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 348);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Crud con Seria-Deseria/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Crud_con_Seria_Deseria
+{
+    public partial class Form1 : Form
+    {
+        Lista personelista = new Lista();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var file = new FileStream("Persone.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader lettore = new StreamReader(file);
+            string riga = lettore.ReadLine();
+            while (riga != null)
+            {
+                string[] dati = riga.Split(' ');
+                Persone persone = new Persone(dati[0], dati[1]);
+                personelista.lista.Add(persone);
+                riga = lettore.ReadLine();                 
+            }
+            Refreshing();
+            lettore.Close();
+            file.Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            var fs = new FileStream("Persone.txt", FileMode.Truncate);
+            fs.Close();
+            var file = new FileStream("Persone.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(file);
+            for (int r = 0; r < personelista.lista.Count; r++)
+            {
+                sw.WriteLine($"{personelista.lista[r].nome} {personelista.lista[r].cognome}");
+            }
+            sw.Close();
+            file.Close();
+        }
+
+        class Lista
+        {
+            public List<Persone> lista = new List<Persone>();
+        }
+
+        class Persone
+        {
+            public Persone(string nome, string cognome)
+            {
+                this.nome = nome;
+                this.cognome = cognome;
+            }        
+
+            public string nome;
+            public string cognome;
+
+            public string Nome
+            {
+                get { return nome; }
+                set { nome = value; }
+            }
+
+            public string Cognome
+            {
+                get { return cognome; }
+                set { cognome = value; }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             Persone persone = new Persone(textBox1.Text, textBox2.Text);
+             personelista.lista.Add(persone);
+             Refreshing();
+             textBox1.Clear();
+             textBox2.Clear();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = listBox1.SelectedIndex;
+            personelista.lista[a].nome = textBox1.Text;
+            personelista.lista[a].cognome = textBox2.Text;
+            textBox1.Clear();
+            textBox2.Clear();
+            Refreshing();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a = listBox1.SelectedIndex;
+            personelista.lista.RemoveAt(a);
+            Refreshing();
+
+        }
+
+        public void Refreshing()
+        {
+            listBox1.Items.Clear();
+            for (int r = 0; r < personelista.lista.Count; r++)
+            {
+                listBox1.Items.Add($"{personelista.lista[r].nome} {personelista.lista[r].cognome}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int r = 0; r < personelista.lista.Count; r++)
+            {
+                if (personelista.lista[r].nome == textBox1.Text && personelista.lista[r].cognome == textBox2.Text)
+                {
+                    listBox1.SelectedIndex = r;
+                }
+            }
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
